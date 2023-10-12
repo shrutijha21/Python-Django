f0ffb09c07393ba21ff975a08a42245d54e8eb19
shrutijha21 <88326166+shrutijha21@users.noreply.github.com>
2023-10-12 15:12:42 +0530

    changes in my_site

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="post-page"),
-    path("posts/<slug>", views.post_detail, name="post-detail-page"),
-]
+from django.urls import path 
+from . import views 
+urlpatterns = [ 
+    path("", views.starting_page, name="starting-page"), 
+    path("posts", views.posts, name="post-page"), 
+    path("posts/<slug>", views.post_detail, name="post-detail-page"), 
+] 
